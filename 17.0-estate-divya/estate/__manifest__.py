227e84fc9d2509ae6e1df4f0ae38e6745a989add
divyasolanki93 <divya.solanki@aaravsolutions.com>
2024-07-23 14:21:10 +0530

    [ADD]estate module

---
+++ 17.0-estate-divya/estate/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "Real Estate",
+    "Summary":"Test Module",
+    'version': "17.0.0.0.0",
+    'depends': ['crm'],
+    'author': "Divya Solaki",
+    'license': 'OEEEL-1',
+    # data files always loaded at installation
+    'data': [
+      
+
+    ],
+    'application': True,
+    
+}
